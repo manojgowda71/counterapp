b1218b4c6addab7e23e5eedeb359e29c42f3b3f3
Manoj k v <95431897+manojgowda71@users.noreply.github.com>
2023-09-21 13:25:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function increment() {
     count += 1
     countEl.textContent = count
 }
+function decrement(){
+    count--;
+    countEl.textContent = count    
+}
 
 function save() {
     let countStr = count + " - "
